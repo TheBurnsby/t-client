af543dd2194f156b0a0c289849d345423db8fc20
Jon Burns <jonburns10@gmail.com>
2026-03-24 09:36:32 -0700

    new nav component

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { page } from '$app/stores';
+
+  /**
+   * @typedef {Object} NavPage
+   * @property {string} label - The display name for the navigation item
+   * @property {string} path - The URL path for the navigation item
+   */
+
+  /**
+   * @type {NavPage[]}
+   */
+  export let pages = [];
+</script>
+
+<nav class="flex justify-center">
+  <ul class="flex flex-row overflow-x-auto [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
+    {#each pages as navPage}
+      {@const isActive = $page.url.pathname === navPage.path}
+      <li>
+        <a
+          href={navPage.path}
+          class="block whitespace-nowrap px-4 py-2 text-sm font-medium transition-colors
+            {isActive
+              ? 'bg-gray-900 text-white rounded'
+              : 'text-gray-600 hover:text-gray-900'}"
+        >
+          {navPage.label}
+        </a>
+      </li>
+    {/each}
+  </ul>
+</nav>
